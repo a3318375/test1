1081e5b7cc30bb51d90284f672e074b1b674e83e
a3318375 <184608371@qq.com>
2015-08-21 10:32:43 +0800

    1

--- src/com/yuxh/test1/ABCTest.java
+++ src/com/yuxh/test1/ABCTest.java
@@ -2,10 +2,14 @@ package com.yuxh.test1;
 
 import org.junit.Test;
 
-public class Test1 {
+public class ABCTest {
 
 	@Test
 	public void test(){
 		System.out.println("32132132132131");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(112321321);
 	}
 }
